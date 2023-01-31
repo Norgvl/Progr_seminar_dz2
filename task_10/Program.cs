ce3fed41e9bc2b44acc2a7c8500a88026f6ea5b0
vlad <norg.vl@mail.ru>
2023-01-31 15:57:34 +0300

    firstPart

---
+++ task_10/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.Clear();
+int a = new Random().Next(100, 1000);
+
+int[] Splitting(int n)
+{
+    int[] arr = new int[3];
+    int a1 = n / 10;     // десятки
+    int a2 = n % 100;     // единицы
+    arr[0] = n / 100;     // сотни
+    arr[1] = a1 - arr[0] * 10; // десятки
+    arr[2] = a2 - arr[1] * 10;  // единицы
+    return arr;
+
+}
+
+int[] split = Splitting(a);
+Console.WriteLine(a + " " + split[0] + " " + split[1] + " " + split[2]);
+Console.WriteLine(split[1]);
---
+++ task_15/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] array = new string[7];
+array[0] = "Нет";     //Понедельник
+array[1] = "Нет";     //"Вторник"
+array[2] = "Нет";    //"среда"
+array[3] = "Нет";     //"Четверг"
+array[4] = "Нет";     //"Пятница"
+array[5] = "Да";     //"Суббота"
+array[6] = "Да";    //"Воскресенье"
+
+int dayOfWeek = int.Parse(Console.ReadLine());
+if (dayOfWeek > 7 || dayOfWeek < 1) Console.WriteLine($"{dayOfWeek} не день недели");
+else Console.WriteLine(array[dayOfWeek-1]);
