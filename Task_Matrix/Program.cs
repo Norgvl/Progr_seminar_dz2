a76a3bc33bf01e2451df4362007a3cca707619d9
vlad <norg.vl@mail.ru>
2023-02-01 21:23:59 +0300

    dop_tasks

---
+++ Task_Matrix/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Произведение матриц
+(Время: 1 сек.Память: 16 Мб Сложность: 21 %)
+Заданы две целочисленные матрицы A и B.
+    Матрица A состоит из N строк и M столбцов, Матрица B состоит из M строк и P столбцов. Требуется вычислить произведение данных матриц A*B.*/
+
+int N = int.Parse(Console.ReadLine());
+int M = int.Parse(Console.ReadLine());
+int P = int.Parse(Console.ReadLine());
+int[,] matrixA = new int[N, M];
+int[,] matrixB = new int[M, P];
+
+for (int i = 0; i < N; i++)
+{
+    for(int j = 0; j < M; j++)
+    {
+        matrixA[i, j] = new Random().Next(1,10);
+        Console.Write(matrixA[i, j]+" ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < M; i++)
+{
+    for (int j = 0; j < P; j++)
+    {
+        matrixB[i, j] = new Random().Next(1, 10);
+        Console.Write(matrixB[i, j]+" ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+int[,] productOfMatrix = new int[N, P];
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < P; j++)
+    {
+        for (int k = 0; k < M; k++)
+        {
+            int prod = matrixA[i, k] * matrixB[k, j];
+            productOfMatrix[i, j] = productOfMatrix[i, j] + prod;
+        }
+     Console.Write(productOfMatrix[i, j] + " ");
+        
+    }
+    Console.WriteLine(" ");
+}
---
+++ Task_coins/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+int N = new Random().Next(1, 101);
+int[] arr = new int[N];
+int numberOfCoins = 0;
+int numberOf1 = 0;
+int numberOf0 = 0;
+for (int i = 0; i < N; i++)
+{
+    arr[i] = new Random().Next(0, 2);
+    if (arr[i] == 0)
+    {
+        numberOf0++;
+
+    }
+    else
+    {
+        numberOf1++;
+    }
+}
+
+if (numberOf1 > numberOf0)
+{
+    numberOfCoins = numberOf0;
+}
+else
+{
+    numberOfCoins = numberOf1;
+}
+
+Console.WriteLine(numberOfCoins);
