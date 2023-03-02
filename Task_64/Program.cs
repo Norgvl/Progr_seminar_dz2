5b4babe2dfb3c7d142e83c845438f1722409a31c
vlad <norg.vl@mail.ru>
2023-03-02 16:40:53 +0300

    sem_hw_9

---
+++ Task_64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+Console.Write("N: ");
+int N = int.Parse(Console.ReadLine());
+Numbers(N);
+void Numbers(int n)
+{
+    if (n >= 1)
+    {
+        Console.Write(n+" ");
+        n--;
+        Numbers(n);
+        
+    }
+}
---
+++ Task_66/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+Console.Write("M: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("N: ");
+int N = int.Parse(Console.ReadLine());
+if (N<M) 
+{
+    Console.WriteLine("N должно быть больше M");
+    return;
+}
+Console.WriteLine(Sum(M,N));
+
+int Sum(int M, int N)
+{
+    int sum = 0;
+    if(N>=M)
+    {
+        sum = M;
+        M++;
+        return sum + Sum(M,N);
+
+    }
+    return sum;
+
+}
